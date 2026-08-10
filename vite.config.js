e5f96fc1380833e3b74860e9504794bce6415d49
TusharVis <tusharvish1@gmail.com>
2026-08-11 02:27:24 +0530

    Support GitHub Pages and Render deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tailwindcss(),
   ],
 
-  base: "/Doctor-clinic/",
-});
+  base:
+    mode === "github"
+      ? "/Doctor-clinic/"
+      : "/",
+}));
